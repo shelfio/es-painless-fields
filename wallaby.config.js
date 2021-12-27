bb1a377508db63edf2aa15a534060a7515da09ad
Vlad Holubiev <golubev.vld@gmail.com>
2021-12-27 23:02:02 +0200

    test: add babel config for unit tests

---
+++ wallaby.config.js
@@ -0,0 +1,41 @@
+const babel = require('@shelf/babel-config/backend');
+
+module.exports = wallaby => {
+  return {
+    testFramework: 'jest',
+    files: [
+      'package.json',
+      'src/**/*.ts',
+      'src/**/*.js',
+      'src/**/*.json',
+      'src/package.json',
+      '!src/**/*.test.ts',
+      '!src/**/*.es.ts',
+      '!src/**/*.integration.ts',
+    ],
+    tests: ['src/**/*.test.ts', '!src/**/*.es.test.ts', '!src/**/*.integration.test.ts'],
+    env: {
+      type: 'node',
+      runner: 'node',
+      params: {
+        env: 'TZ=UTC',
+      },
+    },
+    compilers: {
+      'src/**/*.js': wallaby.compilers.babel(),
+      '**/*.ts?(x)': wallaby.compilers.typeScript(),
+    },
+    preprocessors: {
+      '**/*.js': file =>
+        require('@babel/core').transform(file.content, {
+          sourceMap: true,
+          compact: false,
+          filename: file.path,
+          ...babel,
+        }),
+    },
+    setup(w) {
+      w.testFramework.configure({});
+    },
+  };
+};
