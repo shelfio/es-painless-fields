46cc9372b030ac0c5aed38204dc7b50e7085f7ad
Vlad Holubiev <3817380+vladholubiev@users.noreply.github.com>
2025-11-21 17:09:13 +0100

    chore: update eslint config

--- jest-es-config.cjs
+++ jest-es-config.cjs
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
 const mappings = require('./mappings.json');
 
 module.exports = () => {
